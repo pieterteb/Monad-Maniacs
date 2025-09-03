04e88d4551aff1876508d90f6f22ff522a7e8887
Pieter te Brake <pieterteb@gmail.com>
2025-09-03 13:04:44 +0200

    h

--- data_structures/segment_tree_standard.cpp
+++ data_structures/segment_tree_standard.cpp
@@ -1,47 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-/* Segment tree struct. */
+/* Segment Tree data structure. */
 template<typename T>
-struct SegmentTree {
-    int element_count;              // Number of elements.
-    function<T(T, T)> operation;    // Associative operation.
-    const T identity;               // Value such that operation(a, identity) == a == operation(identity, a) for all valid a.
-    vector<T> tree;                 // Stores the binary tree.
+struct SegTree {
+    int elementc;
+    vector<T> tree; // Root has index 1; the 0th element is unused.
+    
+    // CHANGE THIS SEGMENT
+    const T id = 0;
+    T operation(T a, T b) {
+        return a + b;
+    }
+    // UNTIL HERE
 
-    /* Build a segment tree with n elements and initializes them to id, with operation op and identity i. */
-    SegmentTree(int n, function<T(T, T)> op, T id) : element_count(n), operation(op), identity(id), tree(2 * n, id) {}
+    /* Build a segment tree with n elements and initializes them to id. */
+    SegTree(int n) : elementc(n), tree(2 * n, id) {}
 
-    /* Build a segment tree from arr with operation op and identity id. */
-    SegmentTree(vector<T>& arr, function<T(T, T)> op, T id) : element_count(arr.size()), operation(op), identity(id), tree(2 * arr.size()) {
-        copy(arr.begin(), arr.end(), tree.begin() + element_count);
-        for (int i = element_count - 1; i > 0; --i)
+    /* Build a segment tree from arr. */
+    SegTree(vector<T>& arr) : elementc(arr.size()), tree(2 * arr.size(), id) {
+        copy(arr.begin(), arr.end(), tree.begin() + elementc);      // Copy the array values to the bottom of the tree
+        for (int i = elementc - 1; i > 0; --i)
             tree[i] = operation(tree[2 * i], tree[2 * i + 1]);
     }
+
+    /* Returns cumulative result of applying segtree.operation on [left, right). Returns segtree.identity if the interval is empty. */
+    T query(unsigned left, unsigned right) {
+        T result = id;
+        left += elementc;
+        right += elementc;
+        while (left < right) {
+            if (left & 1) result = operation(result, tree[left++]);
+            if (right & 1) result = operation(result, tree[--right]);
+            left >>= 1; // Dividing by 2
+            right >>= 1;
+        }
+        return result;
+    }
+
+    /* Assigns value to element at index. */
+    void assign(unsigned index, T value) {
+        index += elementc;
+        tree[index] = value;
+        while (index > 1) {
+            tree[index >> 1] = operation(tree[index], tree[index ^ 1]);
+            index >>= 1;
+        }
+    }
 };
-
-/* Returns cumulative result of applying segtree.operation on [left, right). Returns segtree.identity if the interval is empty. */
-template<typename T>
-T segment_tree_query(SegmentTree<T>& segtree, unsigned left, unsigned right) {
-    T result = segtree.identity;
-    left += segtree.element_count;
-    right += segtree.element_count;
-    while (left < right) {
-        if (left & 1) result = segtree.operation(result, segtree.tree[left++]);
-        if (right & 1) result = segtree.operation(result, segtree.tree[--right]);
-        left >>= 1;
-        right >>= 1;
-    }
-    return result;
-}
-
-/* Assigns value to element at index. */
-template<typename T>
-void segment_tree_assign(SegmentTree<T>& segtree, unsigned index, T value) {
-    index += segtree.element_count;
-    segtree.tree[index] = value;
-    while (index > 1) {
-        segtree.tree[index >> 1] = segtree.operation(segtree.tree[index], segtree.tree[index ^ 1]);
-        index >>= 1;
-    }
-}
--- test_solutions/segment_tree.cpp
+++ test_solutions/segment_tree.cpp
@@ -3,4 +3,68 @@
 using namespace std;
 
 
+/* Segment Tree data structure. */
+template<typename T>
+struct SegTree {
+    int elementc;
+    vector<T> tree; // Root has index 1; the 0th element is unused.
+    
+    // CHANGE THIS SEGMENT
+    const T id = -INFINITY;
+    T operation(T a, T b) {
+        return max(a, b);
+    }
+    // UNTIL HERE
 
+    /* Build a segment tree with n elements and initializes them to id. */
+    SegTree(int n) : elementc(n), tree(2 * n, id) {}
+
+    /* Build a segment tree from arr. */
+    SegTree(vector<T>& arr) : elementc(arr.size()), tree(2 * arr.size(), id) {
+        copy(arr.begin(), arr.end(), tree.begin() + elementc);      // Copy the array values to the bottom of the tree
+        for (int i = elementc - 1; i > 0; --i)
+            tree[i] = operation(tree[2 * i], tree[2 * i + 1]);
+    }
+
+    /* Returns cumulative result of applying segtree.operation on [left, right). Returns segtree.identity if the interval is empty. */
+    T query(unsigned left, unsigned right) {
+        T result = id;
+        left += elementc;
+        right += elementc;
+        while (left < right) {
+            if (left & 1) result = operation(result, tree[left++]);
+            if (right & 1) result = operation(result, tree[--right]);
+            left >>= 1; // Dividing by 2
+            right >>= 1;
+        }
+        return result;
+    }
+
+    /* Assigns value to element at index. */
+    void assign(unsigned index, T value) {
+        index += elementc;
+        tree[index] = value;
+        while (index > 1) {
+            tree[index >> 1] = operation(tree[index], tree[index ^ 1]);
+            index >>= 1;
+        }
+    }
+};
+
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+    SegTree st(arr);
+
+    
+
+    return 0;
+}
