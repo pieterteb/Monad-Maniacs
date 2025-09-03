a9b51e2a2bb2220aba5ecfd3d6bb8090b561e891
RobQnadqs <robertbol@upcmail.nl>
2025-09-03 12:06:29 +0200

    .

--- contest.cpp
+++ contest.cpp
@@ -13,36 +13,33 @@ struct UF {
     UF(int n) : par(n), rank(n, 0), setc(n) {
         for (int i = 0; i < n; ++i) par[i] = i;
     };
+
+    int root(int u) {
+        int r = par[u];
+        if (u == r) return r;
+        return par[u] = root(r);
+    }
+
+    void unite(int u, int v) {
+        int ru = root(u);
+        int rv = root(v);
+        if (ru == rv) return;
+        --setc;
+        if (rank[ru] < rank[rv]) {
+            par[ru] = rv;
+        } else if (rank[ru] > rank[rv]) {
+            par[rv] = ru;
+        } else {
+            par[rv] = ru;
+            ++rank[ru];
+        }
+    }
+
+    bool same_set(int u, int v) {
+        return root(u) == root(v);
+    }
 };
 
-int uf_root(UF& uf, int u) {
-    int r = uf.par[u];
-    if (u == r) return r;
-    return uf.par[u] = uf_root(uf, r);
-}
-
-void uf_union(UF& uf, int u, int v) {
-    int ru = uf_root(uf, u);
-    int rv = uf_root(uf, v);
-    if (ru == rv) return;
-    --uf.setc;
-    if (uf.rank[ru] < uf.rank[rv]) {
-        uf.par[ru] = rv;
-    } else if (uf.rank[ru] > uf.rank[rv]) {
-        uf.par[rv] = ru;
-    } else {
-        uf.par[rv] = ru;
-        ++uf.rank[ru];
-    }
-}
-
-bool uf_same_set(UF& uf, int u, int v) {
-    return uf_root(uf, u) == uf_root(uf, v);
-}
-
-int uf_setc(UF& uf) {
-    return uf.setc;
-}
 
 /* Segment tree struct. */
 template<typename T>
@@ -178,8 +175,8 @@ int kruskal(vector<vector<int>>& edges, int n) {
     int weight = 0;
     UF uf = UF(n);
     for (auto& edge : edges) {
-        if (!uf_same_set(uf, edge[1], edge[2])) {
-            uf_union(uf, edge[1], edge[2]);
+        if (!uf.same_set(edge[1], edge[2])) {
+            uf.unite(edge[1], edge[2]);
             weight += edge[0];
         }
     }
