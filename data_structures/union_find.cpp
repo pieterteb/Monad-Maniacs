03f7f6e15872459ba43b83befe29b6e39f2bb3a5
Pieter te Brake <pieterteb@gmail.com>
2026-02-03 18:06:51 +0100

    format

--- data_structures/union_find.cpp
+++ data_structures/union_find.cpp
@@ -7,22 +7,19 @@ struct UF {
     vector<int> par, rank, size;
     int setc;
     UF(int n) : par(n), rank(n, 0), setc(n), size(n, 1) {
-        for (int i = 0; i < n; ++i)
-            par[i] = i;
+        for (int i = 0; i < n; ++i) par[i] = i;
     }
 
     int root(int u) {
         int r = par[u];
-        if (u == r)
-            return r;
+        if (u == r) return r;
         return par[u] = root(r);
     }
 
     void unite(int u, int v) {
         int ru = root(u);
         int rv = root(v);
-        if (ru == rv)
-            return;
+        if (ru == rv) return;
         --setc;
         if (rank[ru] < rank[rv]) {
             par[ru] = rv;
