be0eb1747d1d95ba407cdcc93e77dea5ca1f8b77
Pieter te Brake <pieterteb@gmail.com>
2026-02-03 18:05:40 +0100

    Fix emergency error exit.

--- data_structures/union_find.cpp
+++ data_structures/union_find.cpp
@@ -6,23 +6,23 @@ using namespace std;
 struct UF {
     vector<int> par, rank, size;
     int setc;
-    UF(int n) : par(n), rank(n, 0), setc(n) {
-        for (int i = 0; i < n; ++i) {
-            par[i]  = i;
-            size[i] = 1;
-        }
+    UF(int n) : par(n), rank(n, 0), setc(n), size(n, 1) {
+        for (int i = 0; i < n; ++i)
+            par[i] = i;
     }
 
     int root(int u) {
         int r = par[u];
-        if (u == r) return r;
+        if (u == r)
+            return r;
         return par[u] = root(r);
     }
 
     void unite(int u, int v) {
         int ru = root(u);
         int rv = root(v);
-        if (ru == rv) return;
+        if (ru == rv)
+            return;
         --setc;
         if (rank[ru] < rank[rv]) {
             par[ru] = rv;
