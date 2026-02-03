72a12fe7b071e77cb71a8485c2b7f2dd95c76683
Pieter te Brake <pieterteb@gmail.com>
2026-02-03 18:04:13 +0100

    UF on steroids.

--- data_structures/union_find.cpp
+++ data_structures/union_find.cpp
@@ -4,11 +4,14 @@ using namespace std;
 
 /* Union Find data structure. */
 struct UF {
-    vector<int> par, rank;
+    vector<int> par, rank, size;
     int setc;
     UF(int n) : par(n), rank(n, 0), setc(n) {
-        for (int i = 0; i < n; ++i) par[i] = i;
-    };
+        for (int i = 0; i < n; ++i) {
+            par[i]  = i;
+            size[i] = 1;
+        }
+    }
 
     int root(int u) {
         int r = par[u];
@@ -23,15 +26,22 @@ struct UF {
         --setc;
         if (rank[ru] < rank[rv]) {
             par[ru] = rv;
+            size[ru] += size[rv];
         } else if (rank[ru] > rank[rv]) {
             par[rv] = ru;
+            size[rv] += size[ru];
         } else {
             par[rv] = ru;
             ++rank[ru];
+            size[rv] += size[ru];
         }
     }
 
     bool same_set(int u, int v) {
         return root(u) == root(v);
+    }
+
+    int set_size(int u) {
+        return size[root(u)];
     }
 };
